9c866bdede0b42c8aa004b685e715b425fed7dcb
严浩 <1424412158@qq.com>
2020-03-06 15:31:48 +0800

    ？

--- paint1/paint1.cpp
+++ paint1/paint1.cpp
@@ -1,12 +1,37 @@
-﻿// paint1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿
 #include <iostream>
+#include <opencv.hpp>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace cv;
+using namespace std;
+int main(int argc, const char * argv[]) {
+
+	cv::Mat dispMat(500, 500, CV_8UC3);
+	cv::Point pt;
+	pt.x = 100;
+	pt.y = 100;
+	circle(dispMat, pt, 50, CV_RGB(255, 0, 0), 1, 8, 0);
+	cv::Point pt1;
+	pt1.x = 10;
+	pt1.y = 10;
+cv:Point pt2;
+	pt2.x = 100;
+	pt2.y = 100;
+	line(dispMat, pt1, pt2, CV_RGB(0, 255, 0), 1, 8, 0);
+	cv::Rect rect;
+	rect.x = 100;
+	rect.y = 100;
+	rect.width = 50;
+	rect.height = 50;
+	rectangle(dispMat, rect, CV_RGB(0, 0, 255), 1, 8, 0);
+	cv::namedWindow("dispMat");
+	cv::imshow("dispMat", dispMat);
+
+	waitKey(0);
+	return 0;
+
 }
+
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
